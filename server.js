2016730ad899d0dea3585a81fa28d7a2740faf17
Ludovic Ivan Horem <ludo.horem@gmail.com>
2017-03-14 13:19:50 +0100

    add entry point for server

---
+++ server.js
@@ -0,0 +1,27 @@
+'use strict';
+
+//Init variables
+const jasmine       = require('jasmine');
+const app           = require('./app');
+const mongoose      = require('mongoose');
+
+//Connect DB
+mongoose.connect('mongodb://127.0.0.1:27017/sgaming');
+
+//Set port
+const PORT = process.env.PORT || 8000;
+
+//Load routes files
+var router = require('./routes/v0.1.routes');
+
+//App definitions
+app.use('/v0.1', router);
+
+app.get('/', function(req, res) {
+    res.end('Welcome to SGaming!');
+});
+
+//Listen app
+app.listen(PORT, function() {
+    console.log('SGaming API listen on port ' + PORT);
+});
